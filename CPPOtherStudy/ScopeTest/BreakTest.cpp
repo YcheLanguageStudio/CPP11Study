37a8216b106f7e0588ac876a24b943b1f3fea7c5
CHE Yulin <yche@cse.ust.hk>
2016-07-26 23:20:03 +0800

    test break proving only with while and switch

---
+++ CPPOtherStudy/ScopeTest/BreakTest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i=0;
+    while (true) {
+        if (true) {
+            if (i == 1) {
+                break;
+            }
+            i++;
+
+        }
+        cout <<"Hello"<<endl;
+    }
+}
