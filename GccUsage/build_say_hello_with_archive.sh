c69e1dd7ee6cebe82c5014cb2707c949819f3230
CHE Yulin <yche@cse.ust.hk>
2016-09-25 20:03:00 +0800

    add update

--- GccUsage/build_say_hello_with_archive.sh
+++ GccUsage/build_say_hello_with_archive.sh
@@ -3,6 +3,6 @@ g++ -c say_util.cpp -o build/say_util.o
 g++ -c say_hello_func.cpp -o build/say_hello_func.o
 #程序 ar 配合参数 -r 创建一个新库 libsay.a 并将命令行中列出的对象文件插入。
 #采用这种方法，如果库不存在的话，参数 -r 将创建一个新的库，而如果库存在的话，将用新的模块替换原来的模块。
-ar -r build/libsay.a build/say_util.o build/say_hello_fuc.o
+ar -r build/libsay.a build/say_util.o build/say_hello_func.o
 ranlib build/libsay.a
 g++ say_hello_main.cpp build/libsay.a -o build/say_hello_main
